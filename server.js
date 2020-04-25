979ff585b3f3016003e44cf73fedc00431acedd3
aman bharti <amanbharti0302@gmail.com>
2020-04-25 23:40:10 +0530

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 
 
 const port = 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT||port, () => {
   console.log(`App running on port ${port}...`);
 });
